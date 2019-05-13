3183c6d999e8e099767d2e089af131449890b0bc
Adrian Menendez <adrian.menendez@amplia.es>
2019-05-13 13:49:25 +0200

    Added test of UPDATE with HTTP

--- src/test/java/hellocucumber/UpdateOperation.java
+++ src/test/java/hellocucumber/UpdateOperation.java
@@ -1,6 +1,5 @@
 package hellocucumber;
 
-import com.jcraft.jsch.*;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
@@ -10,6 +9,7 @@ import hellocucumber.dataStructs.update.UpdateResponseStruct;
 import org.eclipse.paho.client.mqttv3.*;
 
 import java.io.*;
+import java.net.InetSocketAddress;
 import java.net.URI;
 import java.nio.file.Files;
 import java.util.HashMap;
@@ -23,6 +23,8 @@ public class UpdateOperation {
 	private MqttClient client = new MqttClient("tcp://localhost", "ClientSet");
 	private static final String MESSAGE_COMMUNICATION_SUCCESS = "SUCCESSFUL";
 	private static final int PORT_HTTP_SERVER = 9000;
+	private HttpServer server;
+	private static final String PATH_TO_LOCAL_CFG = "./src/test/resources/hellocucumber/es.amplia.oda.datastreams.mqtt.cfg";
 
 	private boolean responseReceived;
 	private boolean responseIsOk;
@@ -32,7 +34,7 @@ public class UpdateOperation {
 	}
 
 	@When("I send a request to ODA to change the configuration")
-	public void iSendARequestToODAToChangeTheConfiguration() throws MqttException, IOException, JSchException {
+	public void iSendARequestToODAToChangeTheConfiguration() throws MqttException, IOException {
 		this.client.connect();
 
 
@@ -42,13 +44,21 @@ public class UpdateOperation {
 		this.responseIsOk = false;
 		this.responseReceived = false;
 
+		server = HttpServer.create(new InetSocketAddress(PORT_HTTP_SERVER), 0);
+		server.createContext("/", new RootHandler());
+		server.createContext("/echoHeader", new EchoHeaderHandler());
+		server.createContext("/echoGet", new EchoGetHandler());
+		server.createContext("/echoPost", new EchoPostHandler());
+		server.setExecutor(null);
+		server.start();
+
 		String temp = "{\"operation\":{\"request\":{\"timestamp\":1557395219834,\"name\":\"UPDATE\",\"parameters\":[{" +
 				"\"name\":\"bundleName\",\"value\":{\"string\":\"oda-smart-energy-test\"}},{\"name\":\"bundleVersion\"," +
 				"\"value\":{\"string\":\"2.0.0\"}},{\"name\":\"deploymentElements\",\"type\":\"string\",\"value\":{" +
 				"\"array\":[{\"name\":\"es.amplia.oda.datastreams.mqtt\",\"version\":\"2.0.0\",\"type\":\"CONFIGURATION" +
 				"\",\"downloadUrl\":\"" +
-				"http://api.opengate.es/south/bundles/deploymentElement/deviceOda/18ae3bdd-8a0b-4a68-9049-03d2a191fcaf/27766837/es.amplia.oda.datastreams.mqtt_2.0.0.cfg" +
-				//"http://localhost:" + PORT_HTTP_SERVER + "/echoGet" +
+				//"http://api.opengate.es/south/bundles/deploymentElement/deviceOda/18ae3bdd-8a0b-4a68-9049-03d2a191fcaf/27766837/es.amplia.oda.datastreams.mqtt_2.0.0.cfg" +
+				"http://localhost:" + PORT_HTTP_SERVER + "/echoGet" +
 				"\",\"path\":\"configuration\",\"order\":1,\"operation\":\"UPGRADE\",\"validators\":[],\"size\":334,\"" +
 				"oldVersion\":\"1.0.0\",\"oldName\":\"es.amplia.oda.datastreams.mqtt\",\"oldPath\":\"configuration\"" +
 				"}]}}],\"id\":\"48589c6e-3d9f-4e59-a066-81f357fb6cf8\"}}}";
@@ -60,6 +70,7 @@ public class UpdateOperation {
 		for(int i = 0; i < 10 && !responseReceived; i++) {
 			TimeUnit.MILLISECONDS.sleep(500);
 		}
+		server.stop(0);
 		client.disconnect();
 		assertTrue(responseIsOk);
 	}
@@ -77,5 +88,43 @@ public class UpdateOperation {
 		}
 		@Override
 		public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {/* method not used*/}
+	}
+
+	private class RootHandler implements HttpHandler {
+		@Override
+		public void handle(HttpExchange httpExchange) {
+			/*method not used*/
+		}
+	}
+
+	private class EchoHeaderHandler implements HttpHandler {
+		@Override
+		public void handle(HttpExchange httpExchange) {
+			/*method not used*/
+		}
+	}
+
+	private class EchoGetHandler implements HttpHandler {
+		@Override
+		public void handle(HttpExchange httpExchange) throws IOException {
+			Map<String, Object> parameters = new HashMap<>();
+			URI requestedUri = httpExchange.getRequestURI();
+			String query = requestedUri.getRawQuery();
+			HttpUtils.parseQuery(query, parameters);
+
+			// send response
+			File file = new File(PATH_TO_LOCAL_CFG);
+			httpExchange.sendResponseHeaders(200, file.length());
+			OutputStream os = httpExchange.getResponseBody();
+			os.write(Files.readAllBytes(file.toPath()));
+			os.close();
+		}
+	}
+
+	private class EchoPostHandler implements HttpHandler {
+		@Override
+		public void handle(HttpExchange httpExchange) {
+			/*method not used*/
+		}
 	}
 }
